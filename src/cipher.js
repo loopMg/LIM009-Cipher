c870a0df5228060397c16a6588d3e47f79c01082
holi <asadasd@ajsdjsad.xom>
2019-02-27 12:50:46 -0500

    function dechipher ok

--- src/cipher.js
+++ src/cipher.js
@@ -11,9 +11,20 @@ window.cipher = {
       newAsciiNum = (asciiNum-65+offset)%26+65;
       result += String.fromCharCode(newAsciiNum);
     }
-    resultado.innerHTML = result;
+    return result;
   },
   decode: (offset, string) => {
+    let result = '';
+    let asciiNum = 0;
+    let newAsciiNum = 0;
+  
+    for (let i = 0; i < string.length; i++) {
+      asciiNum = string.charCodeAt([i]);
+      newAsciiNum = (asciiNum-65-25-offset)%26+90;
+      result += String.fromCharCode(newAsciiNum);
+    }
+    return result;
+  
     /* Acá va tu código */
   }
 };
--- src/index.js
+++ src/index.js
@@ -20,6 +20,15 @@ generalPage.style.display = 'block';
 buttomCipher.addEventListener('click', ()=>{
 const stringText = document.getElementById('string').value;
 const offsetNum = document.getElementById('offset').value;
-let resultado = cipher.encode(offsetNum,stringText);
-resultado.innerHTML = (offsetNum,stringText); 
+const parsear =  parseInt(offsetNum);
+let result = cipher.encode(parsear,stringText);
+resultado.innerHTML = result; 
+})
+
+buttomDecipher.addEventListener('click', ()=>{
+const stringText = document.getElementById('string').value;
+const offsetNum = document.getElementById('offset').value;
+const parsear = parseInt(offsetNum);
+let result = cipher.decode(parsear,stringText);
+resultado.innerHTML = result;
 })
